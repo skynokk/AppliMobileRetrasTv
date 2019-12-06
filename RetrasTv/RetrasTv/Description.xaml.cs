8089d08f93cd3211cd8e5dacf691076fa5fa5050
venomania <bruno.martinsvilasboas@ynov.com>
2019-12-06 15:48:14 +0100

    page description : fonctionalité fini

--- RetrasTv/RetrasTv/Description.xaml.cs
+++ RetrasTv/RetrasTv/Description.xaml.cs
@@ -15,9 +15,52 @@ namespace RetrasTv
         public Description()
         {
             InitializeComponent();
-            var image = new Image { Source = "waterfront.jpg" };
+           
+        }
 
-            image.Source = Device.RuntimePlatform == Device.Android ? ImageSource.FromFile("Test.jpg") : ImageSource.FromFile("Images/Test.jpg");
+        private void ImageButton_Clicked(object sender, EventArgs e)
+        {
+            if (desc3.IsVisible == true || desc2.IsVisible == true || desc4.IsVisible == true)
+            {
+                desc4.IsVisible = false;
+                desc2.IsVisible = false;
+                desc3.IsVisible = false;
+            }
+            desc1.IsVisible = desc1.IsVisible == false;
+        }
+
+        private void ImageButton_Clicked_1(object sender, EventArgs e)
+        {
+            if (desc3.IsVisible == true || desc4.IsVisible == true || desc1.IsVisible == true)
+            {
+                desc1.IsVisible = false;
+                desc4.IsVisible = false;
+                desc3.IsVisible = false;
+            }
+            desc2.IsVisible = desc2.IsVisible == false;
+        }
+
+        private void ImageButton_Clicked_2(object sender, EventArgs e)
+        {
+            if (desc4.IsVisible == true || desc2.IsVisible == true || desc1.IsVisible == true)
+            {
+                desc1.IsVisible = false;
+                desc2.IsVisible = false;
+                desc4.IsVisible = false;
+            }
+            desc3.IsVisible = desc3.IsVisible == false;
+
+        }
+
+        private void ImageButton_Clicked_3(object sender, EventArgs e)
+        {
+            if (desc3.IsVisible == true || desc2.IsVisible == true || desc1.IsVisible == true)
+            {
+                desc1.IsVisible = false;
+                desc2.IsVisible = false;
+                desc3.IsVisible = false;
+            }
+            desc4.IsVisible = desc4.IsVisible == false;
         }
     }
 }
